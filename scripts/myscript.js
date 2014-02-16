3baa4d12d2d518218bf851a818f032cbb1e458b3
ideaofindia <ideaofindia2014@gmail.com>
2014-02-16 15:45:36 +0530

    Create myscript.js

---
+++ scripts/myscript.js
@@ -0,0 +1,9 @@
+function populateiFrame()
+{   
+
+
+//var iframeLive = document.getElementById("iframeLiveHangout");
+//iframeLive.src = "http://www.youtube.com/embed/ph8F320Bljc";  
+
+                            
+}
